ecf251507c21c64f61fc09f9044c09845a41adc7
Xsillery12 <daffafachreza20@gmail.com>
2025-01-19 21:50:44 +0700

    editing start.blade.php

--- resources/views/components/start.blade.php
+++ resources/views/components/start.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('{{ asset('assets/img/footer-apotek.webp') }}') }}">
+    <link rel="icon" href="{{ asset('assets/img/footer-apotek.webp') }}">
     <title>Apotek Huda</title>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
     @vite('resources/css/app.css')
