4a97dff5b5bbd69fea2e15b404ab2736f5285944
Xsillery12 <daffafachreza20@gmail.com>
2025-01-20 01:07:58 +0700

    back to default code

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: process.env.PORT || 5173, // Gunakan PORT dari environment, fallback ke 5173
-    },
 });
